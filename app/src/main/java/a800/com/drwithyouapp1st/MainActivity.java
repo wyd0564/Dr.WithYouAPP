075904654cfbcc664c0cfb3d1ac6062652868a32
chp1995 <405593951@qq.com>
2017-03-24 10:41:38 +0800

    develop0

---
+++ app/src/main/java/a800/com/drwithyouapp1st/MainActivity.java
@@ -0,0 +1,13 @@
+package a800.com.drwithyouapp1st;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
